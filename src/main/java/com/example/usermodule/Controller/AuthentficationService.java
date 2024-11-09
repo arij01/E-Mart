8487ee9bd9dac1e9b959804c9a461d46c4fbe04e
YassinBouziri <bouziriyassin3@gmail.com>
2024-11-09 12:33:36 +0100

    auth

--- src/main/java/com/example/usermodule/Controller/AuthentficationService.java
+++ src/main/java/com/example/usermodule/Controller/AuthentficationService.java
@@ -7,6 +7,7 @@ import com.example.usermodule.Security.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -29,6 +30,15 @@ public class AuthentficationService {
         userRepository.save(user);
         var jwtToken = jwtService.generateToken(user);
         return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        var user=userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return  AuthenticationResponse.builder().token(jwtToken).build();
+
+
+
     }
 
 }
